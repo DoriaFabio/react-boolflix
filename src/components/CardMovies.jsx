2e472f980a6a0cb07c0b55fb8b4f0397d7d4f72a
DoriaFabio <doriafabio2001@gmail.com>
2025-01-14 21:06:13 +0000

    overlay card at hover

--- src/components/CardMovies.jsx
+++ src/components/CardMovies.jsx
@@ -8,12 +8,12 @@ export default function Card({ media }) {
     ? media.original_language + ".png"
     : "placeholder.jpg";
   return (
-    <div className="card">
+    <div className={`${style.cardImg} card`}>
       <img
         src={imgPath + media.poster_path}
         alt={media.title}
       />
-      <div className="card-body bg-dark">
+      <div className={`${style.cardContent} card-body`}>
         <h5 className="card-title text-white">{media.title}</h5>
         <p className="card-text text-white">{media.overview}</p>
         <div className="d-flex justify-content-center">
--- src/components/CardSeries.jsx
+++ src/components/CardSeries.jsx
@@ -4,23 +4,23 @@ const imgPath = import.meta.env.VITE_IMG_PATH;
 
 const flags = ["de", "en", "es", "it", "fr", "ja"];
 export default function Card({ media }) {
-  const flag = flags.includes(media.original_language)
-    ? media.original_language + ".png"
-    : "placeholder.jpg";
-  return (
-    <div className="card">
-      <img
-        src={imgPath + media.poster_path}
-        alt={media.name}
-      />
-      <div className="card-body bg-dark">
-        <h5 className="card-title text-white">{media.name}</h5>
-        <p className="card-text text-white">{media.overview}</p>
-        <div className="d-flex justify-content-center">
-          <img src={`${flag}`} alt={flag} className={`${style.flag}`} />
+    const flag = flags.includes(media.original_language)
+        ? media.original_language + ".png"
+        : "placeholder.jpg";
+    return (
+        <div className={`${style.cardImg} card`}>
+            <img
+                src={imgPath + media.poster_path}
+                alt={media.name}
+            />
+            <div className={`${style.cardContent} card-body`}>
+                <h5 className="card-title text-white">{media.name}</h5>
+                <p className="card-text text-white">{media.overview}</p>
+                <div className="d-flex justify-content-center">
+                    <img src={`${flag}`} alt={flag} className={`${style.flag}`} />
+                </div>
+                {/* <div className={style.cardStar}>{drawStars()}</div> */}
+            </div>
         </div>
-        {/* <div className={style.cardStar}>{drawStars()}</div> */}
-      </div>
-    </div>
-  )
+    )
 }
