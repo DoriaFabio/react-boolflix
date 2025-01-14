2a0068f574bc15a8182ba9834383cd68d3f83649
DoriaFabio <doriafabio2001@gmail.com>
2025-01-14 15:26:35 +0000

    do headerComponent with title and search

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react"
+import { useGlobalContext } from "../context/GlobalContext"
+
 function HeaderComponent() {
+  const {search} = useGlobalContext();
+  const [query, setQuery] = useState("");
+
+  function handleInput(e) {
+    setQuery(e.target.value);
+  }
+  function handleSearch(e) {
+    e.preventDefault();
+    search(query);
+  }
+
   return (
     <header className='d-flex justify-content-between align-items-center text-white bg-black'>
-        <h1 className="mx-2">Boolflix</h1>
-        <p className="mx-2">Ricerca</p>
+        <h1 className="mx-2 text-uppercase mytitle">Boolflix</h1>
+        <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Cerca un titolo"
+              aria-label="Search"
+              name="query"
+              id="query"
+              onChange={handleInput}
+            />
+            <button
+              className="btn btn-danger"
+              type="search"
+              onClick={handleSearch}
+            >
+              Cerca
+            </button>
+          </form>
     </header>
   )
 }
