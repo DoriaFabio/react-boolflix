37d718d6b765163d1a48ec90f3dc334483963dff
DoriaFabio <doriafabio2001@gmail.com>
2025-10-13 12:51:19 +0200

    handleSearch

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,39 +1,10 @@
-import { useState } from "react"
-import { useGlobalContext } from "../context/GlobalContext"
+import HandleSearch from "./handleSearch";
 
 function HeaderComponent() {
-  const { search } = useGlobalContext();
-  const [query, setQuery] = useState("");
-
-  function handleInput(e) {
-    setQuery(e.target.value);
-  }
-  function handleSearch(e) {
-    e.preventDefault();
-    search(query);
-  }
-
   return (
     <header className='flex justify-between items-center text-white bg-black'>
       <img src="./Netflix_logo.png" alt="Logo Netflix" className="ml-5 my-5 w-[150px] h-auto" />
-      <form className="flex mx-3" role="search">
-        <input
-          className="form-control mr-5"
-          type="search"
-          placeholder="Cerca un titolo"
-          aria-label="Search"
-          name="query"
-          id="query"
-          onChange={handleInput}
-        />
-        <button
-          className=""
-          type="search"
-          onClick={handleSearch}
-        >
-          Cerca
-        </button>
-      </form>
+      <HandleSearch />
     </header>
   )
 }
--- src/components/handleSearch.jsx
+++ src/components/handleSearch.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useCallback } from "react";
+import { useGlobalContext } from "../context/GlobalContext";
 
-function handleSearch() {
+export default function HandleSearch() {
+  const { search } = useGlobalContext();
+
+  //! Funzione debounce: limita la frequenza con cui viene chiamata una funzione
+  function debounce(callback, delay) {
+    let timer;
+    return (value) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        callback(value);
+      }, delay);
+    }
+  }
+
+  const debounceSearch = useCallback(
+    debounce((value) => {
+      search(value);
+    }, 500),
+    [search]
+  );
+
   return (
-    <div>handleSearch</div>
-  )
+    <input
+      type="text"
+      placeholder="Cerca per titolo"
+      onChange={(e) => debounceSearch(e.target.value)}
+      className="bg-amber-50 rounded-xl text-black p-2 shadow-md shadow-gray-400 mx-10"
+    />
+  );
 }
-
-export default handleSearch
